47b0d865b786ecbadb5988db82a267c7aa81a99d
NotLebedev <artem-itf@yandex.ru>
2022-10-14 15:25:10 +0300

    Add queue get and check operations

--- task-06/queue/code.rkt
+++ task-06/queue/code.rkt
@@ -4,11 +4,24 @@
 
 (provide make-queue front-queue insert-queue! delete-queue! queue? empty-queue?)
 
+(define (get-queue-head q)
+  (mcar (mlist-ref q 1)))
+
+(define (get-queue-tail q)
+  (mcdr (mlist-ref q 1)))
+
+(define (set-queue-head q e)
+  (set-mcar! (mlist-ref q 1) e))
+
+(define (set-queue-tail q e)
+  (set-mcdr! (mlist-ref q 1) e))
+
 (define (make-queue)
-  (mlist 'type$table))
+  (let ([queue-lst (mlist)])
+    (mlist 'type$table (mcons queue-lst queue-lst))))
 
 (define (front-queue q)
-  '())
+  (get-queue-head q))
 
 (define (insert-queue! q e)
   '())
@@ -20,4 +33,4 @@
   (equal? (mlist-ref q 0) 'type$table))
 
 (define (empty-queue? q)
-  '())
+  (null? (get-queue-head q)))
