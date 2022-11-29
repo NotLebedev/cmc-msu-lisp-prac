a5726b9197a6ef10a460440fb2f8792369a1c9a3
NotLebedev <artem-itf@yandex.ru>
2022-11-29 17:43:44 +0300

    Implement test for task 9

--- task-09/tree/test.rkt
+++ task-09/tree/test.rkt
@@ -1,4 +1,50 @@
 #lang racket/base
 
-(require "code.rkt" rackunit)
+(require "code.rkt" rackunit racket/class)
 
+(define root
+  (new Nonempty2Tree%
+       [tag 'root]
+       [data "root"]
+       [left (new Nonempty2Tree%
+                  [tag 'l]
+                  [data "a"]
+                  [left (new Empty2tree%)]
+                  [right (new Nonempty2Tree%
+                              [tag 'lr]
+                              [data "b"]
+                              [left (new Empty2tree%)]
+                              [right (new Empty2tree%)])])]
+       [right (new Nonempty2Tree%
+                   [tag 'r]
+                   [data "c"]
+                   [left (new Nonempty2Tree%
+                              [tag 'rl]
+                              [data "d"]
+                              [left (new Empty2tree%)]
+                              [right (new Nonempty2Tree%
+                                          [tag 'rlr]
+                                          [data "e"]
+                                          [left (new Empty2tree%)]
+                                          [right (new Empty2tree%)])])]
+                   [right (new Nonempty2Tree%
+                               [tag 'rr]
+                               [data "f"]
+                               [left (new Empty2tree%)]
+                               [right (new Empty2tree%)])])]))
+
+(send root printTree)
+(newline)
+(send (send root get-left) printTree)
+(newline)
+(send (send root get-left)set-left! (new Nonempty2Tree%
+                                         [tag 'll]
+                                         [data "T"]
+                                         [left (new Empty2tree%)]
+                                         [right (new Empty2tree%)]))
+(send (send root get-left) printTree)
+(newline)
+(print (send (send (send root get-left) get-left) isEmpty?))
+(newline)
+(print (send (send (send root get-left) get-left) get-data))
+(newline)
