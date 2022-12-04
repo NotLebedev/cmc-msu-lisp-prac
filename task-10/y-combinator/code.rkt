fadc8ff8efaeef150eaa781f23a85027074148d0
NotLebedev <artem-itf@yandex.ru>
2022-12-04 16:28:35 +0300

    Implement task 10

--- task-10/y-combinator/code.rkt
+++ task-10/y-combinator/code.rkt
@@ -6,4 +6,14 @@
   (lambda (f) ((lambda (x) (x x))
                (lambda (g) (f (lambda args (apply (g g) args)))))))
 
-(define (n!!!! i) 0)
+(define n!!!!
+  (lambda (i)
+    ((Y (lambda (f)
+          (lambda (n result)
+            (case n
+              [(0) result]
+              [(1) result]
+              [(2) (* 2 result)]
+              [(3) (* 3 result)]
+              [else (f (- n 4) (* n result))]))))
+     i 1)))
